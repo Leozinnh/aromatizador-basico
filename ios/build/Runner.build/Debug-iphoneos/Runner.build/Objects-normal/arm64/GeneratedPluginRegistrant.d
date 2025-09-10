dc8ce38119d67e4a1f596b1494bb82b6c227c344
Leozinnh <leonardoaf65572005@gmail.com>
2025-09-10 16:27:12 -0300

    ¨

---
+++ ios/build/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/GeneratedPluginRegistrant.d
@@ -0,0 +1,22 @@
+dependencies: \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Modules/module.modulemap \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterHourFormat.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterViewController.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterSceneDelegate.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterEngineGroup.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Debug-iphoneos/Flutter.framework/Headers/Flutter.h
---
+++ ios/build/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/Leozinho/Downloads/aromatizador_BASICO/ios/build/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
